6f0b2de1dfee4cf10c947e1fc44af0a9d6c0d010
Surya1824 <suryateja5347@gmail.com>
2025-05-08 21:42:50 +0530

    Order Management Svc

---
+++ src/main/java/com/surya/order_management_svc/OrderManagementSvcApplication.java
@@ -0,0 +1,13 @@
+package com.surya.order_management_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surya/order_management_svc/OrderManagementSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surya.order_management_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
